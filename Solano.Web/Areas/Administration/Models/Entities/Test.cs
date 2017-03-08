70ce8afd3d508255a85d5fa8535c12911fab85d3
adst2016 <adam.stachura@hotmail.com>
2017-03-08 22:05:17 +0100

    Entities and repositories..

---
+++ Solano.Web/Areas/Administration/Models/Entities/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Infrastructure.DataBase.Entities;
+
+namespace Solano.Web.Areas.Administration.Models.Entities
+{
+    public class Test : EntityWithDescriptionBase
+    {
+
+    }
+}
---
+++ Solano.Web/Areas/Administration/Models/Repositories/TestRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Infrastructure.DataBase.Repositories;
+using Solano.Web.Areas.Administration.Models.Entities;
+
+namespace Solano.Web.Areas.Administration.Models.Repositories
+{
+    public class TestRepository : RepositoryBase<Test>
+    {
+        public TestRepository(global::NHibernate.ISession session) : base(session)
+        {
+        }
+    }
+}
